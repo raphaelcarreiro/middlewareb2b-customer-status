0bd82f8919f60453dcd87211d822ba233836be5b
raphaelcarreiro <raphaelcarreiro@hotmail.com>
2021-12-21 15:23:03 -0300

    configurado conexão com mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomerStatusModule } from './customer-status/customer-status.module';
 
 @Module({
-  imports: [CustomerStatusModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CustomerStatusModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DB_CONNECTION as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    }),
+  ],
 })
 export class AppModule {}
