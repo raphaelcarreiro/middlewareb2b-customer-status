9ad65fb1767476eb1232fd209bd84c6a83da964d
raphaelcarreiro <raphaelcarreiro@hotmail.com>
2021-12-21 14:30:10 -0300

    removido arquivos desnecessários

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CustomerStatusModule } from './customer-status/customer-status.module';
 
 @Module({
   imports: [CustomerStatusModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
